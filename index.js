01ea505db9bd7a8e47f8cf3490a7a25d7f013b91
Swasti1403 <swastijain7@gmail.com>
2021-05-13 22:55:05 +0530

    More changes

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname +"/index.html");
+});
+
+app.get("/login",function(req,res){
+    res.sendFile(__dirname +"/login.html");
+});
+
+app.post("/login",function(req,res){
+    console.log(req.body);
+    res.send("login successful");
+});
+
+app.get("/register",function(req,res){
+    res.sendFile(__dirname +"/register.html");
+});
+
+app.post("/register",function(req,res){
+    console.log(req.body);
+    res.send("Registration successful");
+});
+
+app.get("/forgetPassword",function(req,res){
+    res.sendFile(__dirname +"/forget-password.html");
+});
+
+app.post("/forgetPassword",function(req,res){
+    console.log(req.body);
+    res.send("Password changed");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("server is running on port 3000");
+});
+
+
+
+
+
