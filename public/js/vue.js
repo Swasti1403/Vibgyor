481b33eb5652a4f4bb87d4fadb09016e81fc181c
Swasti1403 <swastijain7@gmail.com>
2021-05-27 21:43:33 +0530

    test

--- public/js/vue.js
+++ public/js/vue.js
@@ -18,8 +18,7 @@ const app = Vue.createApp({
                 {id:4,question:'7+8+9','a':24,'b':11,'c':12,'d':13}],
             answers: [null,null,null,null],
             buttons1: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50],
-            // buttons2: [],
-            // buttons3: [],
+            
             current_question: 0,
             test_started: false,
         }
